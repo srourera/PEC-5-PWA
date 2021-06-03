5aaec628b42351b729e8e8964d963b8511fbc70e
Sergi Rourera <sergirourerallaurado@gmail.com>
2021-06-03 09:16:46 +0200

    Installing @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { PaletteListComponent } from './components/palette-list/palette-list.com
 import { PaletteDetailComponent } from './components/palette-detail/palette-detail.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent, PaletteListComponent, PaletteDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
